e0bade87b029b456e5dea846e2bcb95e8dc33e9e
letan <letan850@gmail.com>
2023-10-19 21:27:39 +0700

    Push Project

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+namespace ServerApplication
+{
+    class Program
+    {
+        private static Thread threadConsole;
+        private static bool consoleRunning;
+
+        static void Main(string[] args)
+        {
+            threadConsole = new Thread(new ThreadStart(ConsoleThread));
+            threadConsole.Start();
+            Network.instance.ServerStart();
+            
+        }
+
+        private static void ConsoleThread()
+        {
+            string line;
+            consoleRunning = true;
+
+            while (consoleRunning)
+            {
+                line = Console.ReadLine();
+
+                if (String.IsNullOrEmpty(line))
+                {
+                    consoleRunning = false;
+                    return;
+                }
+                else
+                {
+                    
+                }
+            }
+        }
+    }
+}
---
+++ ServerSend.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ServerApplication
+{
+    public class ServerSend
+    {
+        public static void SendTCPData(int _toClient, Packet _packet)
+        {
+            _packet.WriteLength();
+            Network.Clients[_toClient].SendData(_packet);
+        }
+
+        private static void SendTCPDataToAll(int _exceptClient,Packet _packet)
+        {
+            _packet.WriteLength();
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i != _exceptClient)
+                {
+                    Network.Clients[i].SendData(_packet);
+                }
+            }
+        }
+        public static void Welcome(int _toClient, string _msg)
+        {
+            using (Packet _packet = new Packet((int)ServerPackets.welcome))
+            {
+                _packet.Write(_msg);
+                _packet.Write(_toClient);
+                SendTCPData(_toClient, _packet);
+                Console.WriteLine("[ID: " + _toClient + "]Send Welcome");
+            }
+        }
+    }
+}
